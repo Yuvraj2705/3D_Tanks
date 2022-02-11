3bd131e06c2393f3e13b39d33cafcebdab8c0f25
Yuvraj2705 <yg27052002@gmail.com>
2022-02-11 15:31:12 +0530

    Tank Cannon Added

--- 3D Tanks/Assets/Scripts/TankShooting.cs
+++ 3D Tanks/Assets/Scripts/TankShooting.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TankShooting : MonoBehaviour
 {
+    [Header("Ui")]
+    [SerializeField] Image chargeBar;
+
     [Header("Prefabs")]
     [SerializeField] Rigidbody bullet;
 
@@ -12,17 +16,86 @@ public class TankShooting : MonoBehaviour
 
     [Header("Fire Settings")]
     [SerializeField] float force = 5;
+    [SerializeField] float backForce = 2;
+    [SerializeField] float UpForce = 2;
     [SerializeField] float destroyAfter = 2;
     [SerializeField] float fireRate;
-    private float timer;
+    private float timer = 0;
     private float spawnTime = 0.1f;
 
     private int gunSwitchCounter = 0;
 
+    private float uniTimer = 0;
+    private float chargeTill = 2;
+    private bool canCharge;
+    private bool canShoot;
+
+    private Rigidbody rb;
+    private Vector3 direction;
+
+    void Awake()
+    {
+        InitVariables();
+    }
+
+    void InitVariables()
+    {
+        canCharge = true;
+        canShoot = false;
+        rb = GetComponent<Rigidbody>();
+    }
+
     void Update()
     {
-        ShootAi();
-        Hacks();
+        //ShootAi();
+        OneShotAi();
+        //Hacks();
+    }
+
+    void OneShotAi()
+    {
+        chargeBar.fillAmount = uniTimer / chargeTill;
+
+        if(Input.GetKey(KeyCode.Z) && canCharge)
+        {
+            uniTimer += Time.deltaTime;
+            if(uniTimer > chargeTill)
+            {
+                canCharge = false;
+                canShoot = true;
+            }
+        }
+
+        if(canShoot && Input.GetKeyDown(KeyCode.X))
+        {
+            foreach(var firePoint in firePoints)
+            {
+                var bulletInstance = Instantiate(bullet, firePoint.position, firePoint.rotation);
+                bulletInstance.AddForce(firePoint.forward * force, ForceMode.Impulse);
+                Destroy(bulletInstance.gameObject,destroyAfter);
+
+                // Revert Force Exerted By Bullet
+                direction = firePoint.forward;
+            }
+            StartCoroutine(TankRecoil());
+            canCharge = true;
+            canShoot = false;
+            uniTimer = 0;
+        }
+
+        if(Input.GetKeyUp(KeyCode.Z))
+        {
+            uniTimer = 0;
+            canCharge = true;
+            canShoot = false;
+        }
+    }
+
+    IEnumerator TankRecoil()
+    {
+        rb.AddForce(-direction * backForce + Vector3.up * UpForce, ForceMode.Impulse);
+        yield return new WaitForSeconds(0.2f);
+        rb.AddForce(direction * (backForce/2) + Vector3.up * UpForce, ForceMode.Impulse);
     }
 
     void ShootAi()
