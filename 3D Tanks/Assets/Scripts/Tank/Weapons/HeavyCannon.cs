7a66c608cc4826eda8c06866dbba9904cb6fbf3a
Yuvraj2705 <yg27052002@gmail.com>
2022-02-12 07:09:34 +0530

    Heavy Cannon Added
    Heavy Cannon Added with new Tank Model and textures.

---
+++ 3D Tanks/Assets/Scripts/Tank/Weapons/HeavyCannon.cs
@@ -0,0 +1,133 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TanksWeapon
+{
+    public class HeavyCannon : MonoBehaviour
+    {
+        #region Serialized Variables
+        [Header("Ui")]
+        [SerializeField] Image chargeBar;
+
+        [Header("Prefabs")]
+        [SerializeField] Rigidbody bullet;
+
+        [Header("FirePoint Transform")]
+        [SerializeField] Transform[] firePoints;
+
+        [Header("Bullet Settings")]
+        [SerializeField] float bulletSpeed = 5;
+        [SerializeField] float destroyBulletAfter = 2;
+        [HideInInspector] float fireRate;
+
+        [Header("Heavy Cannon Settings")]
+        [SerializeField] float backForce = 2;
+        [SerializeField] float UpForce = 1;
+        #endregion
+
+        #region Private Variables
+
+        private float uniTimer = 0;
+        private float chargeTill = 2;
+
+        private bool canCharge;
+        private bool isCharged;
+        private bool canShoot;
+        private bool canCoolDown;
+
+        private Vector3 direction;
+
+        private Rigidbody rb;
+
+        #endregion
+
+        #region InBuiltMethods
+
+        void Awake()
+        {
+            canCharge = true;
+            canShoot = false;
+            isCharged = false;
+
+            rb = GetComponent<Rigidbody>();
+        }
+
+        void Update()
+        {
+            HeavyCannonAi();
+        }
+
+        #endregion
+
+        #region CustomMethods
+
+        void HeavyCannonAi()
+        {
+            chargeBar.fillAmount = uniTimer / chargeTill;
+
+            if (Input.GetKey(KeyCode.Space) && canCharge)
+            {
+                canCoolDown = false;
+                uniTimer += Time.deltaTime;
+                if (uniTimer > chargeTill)
+                {
+                    canCharge = false;
+                    canShoot = true;
+                    isCharged = true;
+                    canCoolDown = false;
+                } 
+            }
+
+            if (canShoot && Input.GetKeyDown(KeyCode.F))
+            {
+                foreach (var firePoint in firePoints)
+                {
+                    var bulletInstance = Instantiate(bullet, firePoint.position, firePoint.rotation);
+                    bulletInstance.AddForce(firePoint.forward * bulletSpeed, ForceMode.Impulse);
+                    Destroy(bulletInstance.gameObject, destroyBulletAfter);
+
+                    // Revert Force Exerted By Bullet
+                    direction = firePoint.forward;
+                }
+                StartCoroutine(TankRecoil());
+                uniTimer = 0;
+                isCharged = false;
+                canCharge = true;
+                canShoot = false;
+                canCoolDown = true;
+            }
+
+            if(canCoolDown)
+            {
+                if(uniTimer <= 0) { return; }
+                uniTimer -= Time.deltaTime;
+            }
+
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                if(isCharged)
+                {
+                    canCoolDown = false;
+                    return;
+                }
+                else
+                {
+                    canCoolDown = true;
+                    canCharge = true;
+                    canShoot = false;
+                }
+            }
+        }
+
+        IEnumerator TankRecoil()
+        {
+            rb.AddForce(-direction * backForce + Vector3.up * UpForce, ForceMode.Impulse);
+            yield return new WaitForSeconds(0.2f);
+            rb.AddForce(direction * (backForce / 2) + Vector3.up * UpForce, ForceMode.Impulse);
+        }
+
+
+        #endregion
+    }
+}
