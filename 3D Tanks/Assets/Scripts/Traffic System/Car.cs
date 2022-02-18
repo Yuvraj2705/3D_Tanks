e88d10e358ef457efd376a5e8f62899506a25baf
Yuvraj2705 <yg27052002@gmail.com>
2022-02-18 19:37:02 +0530

    Traffic System V 0.0.1

---
+++ 3D Tanks/Assets/Scripts/Traffic System/Car.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Car : MonoBehaviour
+{
+    #region Private Variables
+
+    private Vector3 Destination;
+
+    private NavMeshAgent navvy;
+
+    #endregion
+
+    #region Public Variables
+
+    //Variable to Hide
+    public float LocationCount = 0;
+
+    #endregion
+
+    #region In-built Functions
+
+    void Start()
+    {
+        InitVariables();
+        InitSettings();
+    }
+
+    void Update()
+    {
+        GoToDestination();
+        CheckDistance();
+    }
+
+    #endregion
+
+    #region Node Functions
+    #endregion
+
+    #region Custom Functions
+
+    void InitVariables()
+    {
+        navvy = GetComponent<NavMeshAgent>();
+    }
+
+    void InitSettings()
+    {
+        if(LocationCount == 1)
+        {
+            Destination = GameObject.FindGameObjectWithTag("TS").GetComponent<TrafficControl>().EndPointLeft.position;
+            Debug.Log("LeftPosition Set");
+        }
+        else if(LocationCount == 2)
+        {
+            Destination = GameObject.FindGameObjectWithTag("TS").GetComponent<TrafficControl>().EndPointRight.position;
+            Debug.Log("RightPosition Set");
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void GoToDestination()
+    {
+        navvy.SetDestination(Destination);
+    }
+
+    void CheckDistance()
+    {
+        if(Vector3.Distance(transform.position, Destination) < 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    #endregion
+}
